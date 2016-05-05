120f7b6464216a7f14a56740da93711ddfb19abd
carolFrohlich <carol.frohlich@gmail.com>
2016-05-05 09:57:33 -0400

    read params from config,json

--- send_data.py
+++ send_data.py
@@ -4,12 +4,9 @@ import sys
 import os
 sys.path.append("/sw/lib/python2.6/site-packages/")
 import os.path
-
+import json
 
 path = '/home/caroline/Desktop/test/'
-OTCP_IP='127.0.0.1'
-ODATA_TCP_PORT=7953
-OCTRL_TCP_PORT=7961
 
 def files_to_send(repetition, num_channels, num_slices):
 	to_send = []
@@ -21,7 +18,10 @@ def files_to_send(repetition, num_channels, num_slices):
 				to_send.append(f_name)
 	return to_send
 
-def send_data(aquisition_type, tr, num_slices, num_channels, repetition, delay):
+#aquisition_type, tr, num_slices, num_channels, repetition, delay
+def send_data(config_file):
+	#load config file
+	config = json.load(open(config_file))
 
 	print "about to open comm with AFNI"
 	# open the output ports to send data to AFNI
@@ -33,19 +33,19 @@ def send_data(aquisition_type, tr, num_slices, num_channels, repetition, delay):
 		sys.exit(1)
 
 	try:
-		ocs.connect((OTCP_IP,OCTRL_TCP_PORT))
+		ocs.connect((config["ip"], config["control_port"]))
 	except Exception, e:
-		print 'Connect %s (%d) failed! %s'%(OTCP_IP,OCTRL_TCP_PORT,e)
+		print 'Connect %s (%d) failed! %s'%(config["ip"], config["control_port"],e)
 		ocs.close()
 		sys.exit(1)
 
-	print 'sending tcp:%s:%d\x00\n'%(OTCP_IP,ODATA_TCP_PORT)
+	print 'sending tcp:%s:%d\x00\n'%(config["ip"], config["data_port"])
 
 	# send connection string to AFNI
 	try:
-		ocs.send('tcp:%s:%d\x00'%(OTCP_IP,ODATA_TCP_PORT))
+		ocs.send('tcp:%s:%d\x00'%(config["ip"], config["data_port"]))
 	except Exception, e:
-		print 'send to %s (%d) failed! %s'%(OTCP_IP,OCTRL_TCP_PORT,e)
+		print 'send to %s (%d) failed! %s'%(config["ip"], config["data_port"],e)
 		ocs.close()
 		sys.exit(1)
 	ocs.close()
@@ -66,10 +66,10 @@ def send_data(aquisition_type, tr, num_slices, num_channels, repetition, delay):
 	ocf_count=0
 	while ocf==False:
 		try:
-			ods.connect((OTCP_IP,ODATA_TCP_PORT))
+			ods.connect((config["ip"], config["data_port"]))
 			ocf=True
 		except Exception, e:
-			print 'Connect to (%s, %d) failed! %s'%(OTCP_IP,ODATA_TCP_PORT,e)
+			print 'Connect to (%s, %d) failed! %s'%(config["ip"], config["data_port"],e)
 			ocf=False
 			ocf_count=ocf_count+1
 			if( ocf_count > 20 ):
@@ -77,22 +77,25 @@ def send_data(aquisition_type, tr, num_slices, num_channels, repetition, delay):
 				ods.close()
 				sys.exit(1)
 
-	print "Connection (",OTCP_IP,",",ODATA_TCP_PORT,") to AFNI established"
+	print "Connection (",config["ip"],",",config["data_port"],") to AFNI established"
 	# open the input ports to receive data from rtfeedme/scanner
 
-	AFNI_cmd_string="ACQUISITION_TYPE 2D+zt\n"
-	AFNI_cmd_string+="TR %f\n"%(tr)
-	AFNI_cmd_string+="XYFOV %d %d %f\n"%(220,220,144)
-	AFNI_cmd_string+="ZNUM %d\n"%(num_slices)
-	AFNI_cmd_string+="XYZAXES A-P R-L I-S\n"
-	AFNI_cmd_string+="DATUM %s\n"%("short")
-	AFNI_cmd_string+="XYMATRIX %d %d\n"%(64,64)
-	AFNI_cmd_string+="NUM_CHAN %d\n"%(num_channels)
+	fov = config["xyfov"]
+	xym = config["xy_matrix"]
+
+	AFNI_cmd_string="ACQUISITION_TYPE %s\n"%(config["aquisition_type"])
+	AFNI_cmd_string+="TR %f\n"%(config["TR"])
+	AFNI_cmd_string+="XYFOV %d %d %f\n"%(fov[0], fov[1], fov[2])
+	AFNI_cmd_string+="ZNUM %d\n"%(config["num_slices"])
+	AFNI_cmd_string+="XYZAXES %s\n"%(config["xyz_axes"])
+	AFNI_cmd_string+="DATUM %s\n"%(config["datum"])
+	AFNI_cmd_string+="XYMATRIX %d %d\n"%(xym[0],xym[1])
+	AFNI_cmd_string+="NUM_CHAN %d\n"%(config["num_channels"])
 	AFNI_cmd_string+='\0'
 	print "command string",AFNI_cmd_string
 	ods.send(AFNI_cmd_string) 
 
-	to_send = files_to_send(repetition, num_channels, num_slices)
+	to_send = files_to_send(config["repetition"], config["num_channels"], config["num_slices"])
 	for f in to_send:
 		print 'try to send', f
 		while True:
@@ -103,51 +106,9 @@ def send_data(aquisition_type, tr, num_slices, num_channels, repetition, delay):
 					print 'sent',f
 					break
 			else:
-				time.sleep(delay)
+				time.sleep(config["delay"])
 	ods.close()
 
-def parse_params(args):
-	if len(args) != 6:
-		print 'Usage:'
-		print '    python test_img.py aquisition_type, tr, num_slices, num_channels, repetition, delay'
-		print 'aquisition_type: 2D+z, 2D+zt, 3D, 3D+t'
-		print 'tr: int'
-		print 'num_slices: int'
-		print 'num_channels: int'
-		print 'repetition: int'
-		print 'delay: float'
-		print 'example:'
-		print '    python test_img.py 2D+zt 2 48 4 20 0.125'
-		sys.exit()
-
-	else:
-		if args[0] not in ['2D+z', '2D+zt','3D', '3D+t']:
-			print 'aquisition type must be one of the following:'
-			print '2D+z, 2D+zt, 3D, 3D+t'
-			sys.exit()
-
-		try:
-			x = float(args[1])
-		except Exception, e:
-			print 'tr must be int or float'
-			sys.exit()
-		if not args[2].isdigit():
-			print 'num_slices must be integer'
-			sys.exit()
-		if not args[3].isdigit():
-			print 'num_channels must be integer'
-			sys.exit()
-		if not args[4].isdigit():
-			print 'repetition must be integer'
-			sys.exit()
-		try:
-			x = float(args[5])
-		except Exception, e:
-			print 'delay must be float'
-			sys.exit()
-
-		return args[0], float(args[1]), int(args[2]), int(args[3]), int(args[4]), float(args[5])
 
 if __name__ == "__main__":
-	a_type, tr, num_slices, num_channels, repetition, delay = parse_params(sys.argv[1:])
-	send_data(a_type, tr, num_slices, num_channels, repetition, delay)
+	send_data("config.json")
